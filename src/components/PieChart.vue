cc36890b8ff159cec176a546209b3c76c0a06302
“Anthony” <anthony.jackson.002@gmail.com>
2020-02-03 19:09:26 -0500

    add modular component

---
+++ src/components/PieChart.vue
@@ -0,0 +1,20 @@
+<template>
+    <pie-chart :data="chartData"></pie-chart>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      chartData: {
+        '2017-05-13': 2,
+        '2017-05-14': 3,
+        '2017-05-15': 2,
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
