2d0f1a0334bc3fe5ca7d40fec538b6f6e05d2d26
danartzcoder <artz.13@wright.edu>
2020-02-07 15:14:06 -0500

    added custom doughnut piechart. need to fix size issue

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,21 +1,43 @@
 <template>
   <div>
-      <pie-chart :data="chartData"></pie-chart>
+      <chartjs-doughnut v-bind:labels="labels" v-bind:datasets="datasets" v-bind:option="option"></chartjs-doughnut>
   </div> 
 </template>
 
+
 <script>
+
 export default {
-  data() {
-    return {
-      chartData: {
-        'Salary': 45000.00,
-        'Benefits': 15802.00
-      },
-    };
-  },
+   data(){
+     return{
+       labels: ["Salary", "Benefits"],
+     
+     datasets: [
+       {
+         data: [.74, .26],
+         backgroundColor: ["#026937", "#CEA052"],
+         hoverBackgroundColor: ["#02b55e", "#ffc96e"]
+       }
+     ],
+     option: {
+       title: {
+         display: true,
+         position: "top",
+         text: "Total Compensation",
+         
+       },
+       maintainAspectRatio: true,
+       responsive: true
+       
+     }
+   };
+   }
+  
 };
 </script>
 
-<style></style>
+<style>
+
+
+</style>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
-import Chartkick from 'vue-chartkick';
+//import Chartkick from 'vue-chartkick';
 import Chart from 'chart.js';
 import App from './App.vue';
+import "hchs-vue-charts";
 
-Vue.use(Chartkick.use(Chart));
+Vue.use(window.VueCharts);
 Vue.config.productionTip = false;
+
 
 new Vue({
   render: h => h(App),
